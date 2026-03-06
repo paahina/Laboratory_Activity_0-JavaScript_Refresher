afdb64f8b8002286856f08d9110f22cbe8fc4cd3
Page Aurellano <pagepoako@gmail.com>
2026-03-06 19:47:39 +0800

    Finished Laboratory Activity 0

--- scripts/student.js
+++ scripts/student.js
@@ -5,7 +5,7 @@ export class Student {
   constructor(id, name) {
     this.#id = id;
     this.#name = name;
-    this.#isPresent = false;
+    this.#isPresent = "unchecked";
   }
   get name() {
     return this.#name;
@@ -13,9 +13,18 @@ export class Student {
   get isPresent() {
     return this.#isPresent;
   }
+  set isPresent(isPresent) {
+    this.#isPresent = isPresent;
+  }
   get id() {
     return this.#id;
   }
 }
-
-export const students = [new Student(1, "Cyrus Robles")];
+//test
+export const students = [
+  new Student(1, "Cyrus Robles"),
+  new Student(2, "Page Aurellano"),
+  new Student(3, "Gabriel Josh Eugenio"),
+  new Student(4, "Joshua Maquilan"),
+  new Student(5, "Kristine Navarro"),
+];
